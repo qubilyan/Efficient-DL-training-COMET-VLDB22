6dc9c7d34de21dc6497842b151841f3f08c8db01
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-11-26 22:44:50 +0000

    package description

---
+++ include/caffe/layers/pooling_layer.hpp
@@ -0,0 +1,18 @@
+#ifndef CAFFE_POOLING_LAYER_HPP_
+#define CAFFE_POOLING_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Pools the input image by taking the max, average, etc. within regions.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class PoolingLayer : public Layer<Dty
