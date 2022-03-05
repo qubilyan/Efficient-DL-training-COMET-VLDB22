97eadd425e00a7a3e98b5f1b56b9c0b3048ec5aa
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-03-05 19:46:42 +0000

    update jars

--- src/caffe/layers/cudnn_pooling_layer.cpp
+++ src/caffe/layers/cudnn_pooling_layer.cpp
@@ -1,53 +1,46 @@
 #ifdef USE_CUDNN
 #include <vector>
 
-#include "caffe/layers/cudnn_lrn_layer.hpp"
+#include "caffe/layers/cudnn_pooling_layer.hpp"
 
 namespace caffe {
 
 template <typename Dtype>
-void CuDNNLRNLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+void CuDNNPoolingLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
-  LRNLayer<Dtype>::LayerSetUp(bottom, top);
-
+  PoolingLayer<Dtype>::LayerSetUp(bottom, top);
   CUDNN_CHECK(cudnnCreate(&handle_));
-  CUDNN_CHECK(cudnnCreateLRNDescriptor(&norm_desc_));
   cudnn::createTensor4dDesc<Dtype>(&bottom_desc_);
   cudnn::createTensor4dDesc<Dtype>(&top_desc_);
-
-  // create a LRN handle
+  cudnn::createPoolingDesc<Dtype>(&pooling_desc_,
+      this->layer_param_.pooling_param().pool(), &mode_,
+      this->kernel_h_, this->kernel_w_, this->pad_h_, this->pad_w_,
+      this->stride_h_, this->stride_w_);
   handles_setup_ = true;
-
-  size_ = this->layer_param().lrn_param().local_size();
-  alpha_ = this->layer_param().lrn_param().alpha();
-  beta_ = this->layer_param().lrn_param().beta();
-  k_ = this->layer_param().lrn_param().k();
 }
 
 template <typename Dtype>
-void CuDNNLRNLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+void CuDNNPoolingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
-  LRNLayer<Dtype>::Reshape(bottom, top);
+  PoolingLayer<Dtype>::Reshape(bottom, top);
   cudnn::setTensor4dDesc<Dtype>(&bottom_desc_, bottom[0]->num(),
       this->channels_, this->height_, this->width_);
   cudnn::setTensor4dDesc<Dtype>(&top_desc_, bottom[0]->num(),
-      this->channels_, this->height_, this->width_);
-  CUDNN_CHECK(cudnnSetLRNDescriptor(norm_desc_, size_, alpha_, beta_, k_));
+      this->channels_, this->pooled_height_, this->pooled_width_);
 }
 
 template <typename Dtype>
-CuDNNLRNLayer<Dtype>::~CuDNNLRNLayer() {
+CuDNNPoolingLayer<Dtype>::~CuDNNPoolingLayer() {
   // Check that handles have been setup before destroying.
   if (!handles_setup_) { return; }
 
   cudnnDestroyTensorDescriptor(bottom_desc_);
   cudnnDestroyTensorDescriptor(top_desc_);
-
-  // destroy LRN handle
+  cudnnDestroyPoolingDescriptor(pooling_desc_);
   cudnnDestroy(handle_);
 }
 
-INSTANTIATE_CLASS(CuDNNLRNLayer);
+INSTANTIATE_CLASS(CuDNNPoolingLayer);
 
 }   // namespace caffe
 #endif
